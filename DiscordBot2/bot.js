4c2f36b5918b78db73fd1a26051a60e4709a6ce3
Unknown <m.koning02@student.alfa-college.nl>
2018-01-26 10:45:51 +0100

    Botfixes

--- DiscordBot2/bot.js
+++ DiscordBot2/bot.js
@@ -10,3 +10,23 @@ client.on('ready', () => {
 });
 
 
+client.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it needs to execute a command
+    // for this script it will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+        var message = "pong";
+
+        args = args.splice(1);
+
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                client.sendMessage({ to: channelID, message: 'Pong!' });
+                break;
+            default:
+                client.sendMessage({ to: channelID, message: 'Unknown command.' });
+        }
+    }
+})
