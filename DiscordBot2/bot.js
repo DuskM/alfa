c64bee49ba136b9c7e41f039f711a6257174efb1
Unknown <m.koning02@student.alfa-college.nl>
2018-01-26 11:24:09 +0100

    command change

--- DiscordBot2/bot.js
+++ DiscordBot2/bot.js
@@ -13,7 +13,7 @@ client.on('ready', () => {
 client.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it needs to execute a command
     // for this script it will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
+    if (message.substring(0, 1) == '$') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
         var message = "pong";
